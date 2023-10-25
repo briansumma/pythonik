a61e28ec12b343cd20b295320bac653f33b515ac
brant <brant@northshoreautomation.com>
2023-10-25 00:15:09 +0000

    add get asset format fix bug wrong asset format path

--- pythonik/specs/files.py
+++ pythonik/specs/files.py
@@ -4,7 +4,8 @@ from pythonik.models.files.proxy import Proxy
 from pythonik.specs.base import Spec
 
 GET_ASSET_PROXY_PATH = "assets/{}/proxies/{}/"
-GET_ASSETS_FORMATS_PATH = "/v1/assets/{}/formats/"
+GET_ASSETS_FORMATS_PATH = "assets/{}/formats/"
+GET_ASSETS_FORMAT_PATH = "assets/{}/formats/{}"
 
 
 class FilesSpec(Spec):
@@ -22,5 +23,11 @@ class FilesSpec(Spec):
     def get_asset_formats(self, asset_id: str, **kwargs) -> Response:
         """Get all asset's formats"""
         resp = self._get(GET_ASSETS_FORMATS_PATH.format(asset_id), **kwargs)
+
+        return self.parse_response(resp, Format)
+
+    def get_asset_format(self, asset_id: str, format_id: str, **kwargs) -> Response:
+        """Get asset format"""
+        resp = self._get(GET_ASSETS_FORMAT_PATH.format(asset_id, format_id), **kwargs)
 
         return self.parse_response(resp, Format)
--- pythonik/tests/test_files.py
+++ pythonik/tests/test_files.py
@@ -6,6 +6,7 @@ import requests_mock
 
 from pythonik.specs.files import (
     GET_ASSET_PROXY_PATH,
+    GET_ASSETS_FORMAT_PATH,
     GET_ASSETS_FORMATS_PATH,
     FilesSpec,
 )
@@ -27,6 +28,23 @@ def test_get_proxy():
         m.get(mock_address, json=data)
         client = PythonikClient(app_id=app_id, auth_token=auth_token, timeout=3)
         client.files().get_asset_proxy(asset_id, proxy_id)
+
+
+def test_get_asset_format():
+    with requests_mock.Mocker() as m:
+        app_id = str(uuid.uuid4())
+        auth_token = str(uuid.uuid4())
+        asset_id = str(uuid.uuid4())
+        format_id = str(uuid.uuid4())
+
+        model = Format()
+        data = model.model_dump()
+        mock_address = FilesSpec.gen_url(
+            GET_ASSETS_FORMAT_PATH.format(asset_id, format_id)
+        )
+        m.get(mock_address, json=data)
+        client = PythonikClient(app_id=app_id, auth_token=auth_token, timeout=3)
+        client.files().get_asset_format(asset_id, format_id)
 
 
 def test_get_asset_formats():
